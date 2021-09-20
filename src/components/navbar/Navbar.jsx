82539f8264bfdaa47d7d9ae642adf33ff6d735f6
Matias Flores <floresmatias@gmail.com>
2021-09-19 22:20:26 -0300

    add style navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  let logo =
+    "https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg";
   return (
-    <div>
-      <h1>Navbar</h1>
-      <NavLink to="/favorites">Home</NavLink>
-      <NavLink to="/">Favorites</NavLink>
-      <NavLink to="/about">About</NavLink>
+    <div className="navbar">
+      <div>
+        <NavLink to="/">
+          <img src={logo} alt="logo" />
+        </NavLink>
+      </div>
+      <div>
+        <span>
+          <NavLink to="/favorites" activeStyle={{ color: "#0B3D91" }}>
+            Favorites
+          </NavLink>
+        </span>
+        <li></li>
+        <span>
+          <NavLink to="/about" activeStyle={{ color: "#0B3D91" }}>
+            About
+          </NavLink>
+        </span>
+      </div>
+
     </div>
   );
 };
